878390b9887c3d137ba1857d58ac7ea4fe9112da
Sebastien Najjar <sebastien.najjar@gmail.com>
2011-10-20 23:36:40 -0700

    make setenv.sh sourcable from anywhere and make regression works even without sourcing setenv

--- visualvalgrind/test/regression/001_option_build/001_basic/validate.sh
+++ visualvalgrind/test/regression/001_option_build/001_basic/validate.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# find out the directory where this script is
+DIR="$PWD/$(dirname ${BASH_SOURCE[@]})/../../../../.."
+REALDIR=$(readlink -f $DIR)
+LIBDIR="$REALDIR/visualvalgrind/libs"
+
+
 rm -f Leak_DefinitelyLost.dot 2>&1 > /dev/null
-../../../../visualvalgrind.py build valtest.xml
+PYTHONPATH="$LIBDIR" ../../../../visualvalgrind.py build valtest.xml
 test=`diff expected Leak_DefinitelyLost.dot`
 
 if [ "$test" = "" ]; then
-    echo 1
+    echo "1"
 else
-    echo 0
+    echo "0"
 fi
 
 rm -f Leak_DefinitelyLost.dot 2>&1 > /dev/null
