9cb609fd1a8b8ec069d50f42a28d88a1863e0214
Sebastien Najjar <sebastien.najjar@gmail.com>
2011-10-22 03:44:16 -0700

    add regression tests for all options

---
+++ visualvalgrind/test/regression/005_option_output_dir/001_basic/validate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# find out the directory where this script is
+DIR="$PWD/$(dirname ${BASH_SOURCE[@]})/../../../../.."
+REALDIR=$(readlink -f $DIR)
+LIBDIR="$REALDIR/visualvalgrind/libs"
+
+
+rm -f *.dot 2>&1 > /dev/null
+rm -rf output_dir/
+mkdir output_dir
+PYTHONPATH="$LIBDIR" ../../../../visualvalgrind.py -o output_dir build valtest.xml
+
+# test that nothing is outputed in current directory
+test=$(ls *.dot 2> /dev/null)
+if [ "$test" != "" ]; then
+    echo "0"
+    exit 1
+fi
+
+# test that .dot are output in output_dir
+test=$(ls output_dir/*.dot)
+if [ "$test" = "" ]; then
+    echo "0"
+    exit 1
+fi
+
+echo "1"
+
+rm -f *.dot 2>&1 > /dev/null
+rm -rf output_dir/
