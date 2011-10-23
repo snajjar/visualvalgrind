b3702a6654b55255e705ec8fc5f1b72bf5c7142b
Diane Gasselin <d.gasselin@gmail.com>
2011-10-23 18:38:51 +0200

    fix of dictionary implementation and update of reg tests due to order modifications

--- iograph/src/core/IOGraph.py
+++ iograph/src/core/IOGraph.py
@@ -35,22 +35,21 @@ class Arrow_not_creable(Exception):
 class Graph(attribute_object):
     def __init__(self, name = ""):
         self.name = ""
-        self.nodes = []
-        self.arrows = []
+        self.nodes = {}
+        self.arrows = {}
         attribute_object.__init__(self)
 
     def add_node(self, name):
-        self.nodes.append( Node(name) )
+        self.nodes[name] = Node(name)
 
     # return the node object if node exist. Return None otherwise
     def get_node(self, name):
-        for node in self.nodes:
-            if node.get_name() == name:
-                return node
+        if self.has_node(name):
+            return self.nodes[name]
         return None
 
     def has_node(self,name):
-        return self.get_node(name) != None
+        return name in self.nodes.keys()
 
     def add_arrow(self, n1name, n2name, name = ""):
         node1 = self.get_node(n1name)
@@ -64,20 +63,19 @@ class Graph(attribute_object):
         arrow = Arrow(node1, node2, name)
         node1.add_out_arrow(arrow)
         node2.add_in_arrow(arrow)
-        self.arrows.append(arrow)
+        self.arrows[(n1name,n2name)] = arrow
    
     def get_arrow(self, n1name, n2name):
         node1 = self.get_node(n1name)
         node2 = self.get_node(n2name)
         if node1 is None or node2 is None:
             return None
-        for arrow in node1.get_out_arrows():
-            if node2 == arrow.get_dst_node():
-                return arrow
+        if self.has_arrow(n1name,n2name):
+            return self.arrows[(n1name,n2name)]
         return None
 
     def has_arrow(self, n1name, n2name):
-        return self.get_arrow(n1name, n2name) != None
+        return (n1name, n2name) in self.arrows.keys()
 
     def del_arrow(self, n1name, n2name):
         node1 = self.get_node(n1name)
@@ -93,7 +91,7 @@ class Graph(attribute_object):
             raise Arrow_not_deletable(n1name, n2name, "Arrow doesn't exist")
         node1.del_out_arrow(n2name)
         node2.del_in_arrow(n1name)
-        self.arrows.remove(arrow)
+        del self.arrows[(n1name,n2name)]
 
     def del_node(self, node_name):
         node = self.get_node(node_name)
@@ -101,13 +99,13 @@ class Graph(attribute_object):
             raise Node_not_deletable(node_name, "Node not found")
         if node.has_arrows():
             raise Node_not_deletable(node_name, "Node has arrows")
-        self.nodes.remove(node)
+        del self.nodes[node_name]
 
     def get_nodes(self):
-        return self.nodes
+        return self.nodes.values()
 
     def get_arrows(self):
-        return self.arrows
+        return self.arrows.values()
 
 
 
