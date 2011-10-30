9c72a329eb4edb20e2836f9effdcf030a9237094
Sebastien Najjar <sebastien.najjar@gmail.com>
2011-10-30 03:39:26 -0700

    adapt generateExamples.sh

--- visualvalgrind/examples/generateExamples.sh
+++ visualvalgrind/examples/generateExamples.sh
@@ -7,7 +7,7 @@ rm *.svg 2> /dev/null
 # build graph dot file
 for rep in `find . -type d | grep "./"  | sed "s/\.\///" | grep -v "/" | sort`
 do
-    ../visualvalgrind.py build $rep/valtest.xml
+    ../src/visualvalgrind.py build $rep/valtest.xml
     # create svg file
     for dotfile in `ls *.dot` 
     do 
