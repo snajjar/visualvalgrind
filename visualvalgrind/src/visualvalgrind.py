ea6884717d18c818eb6561454562c6cb99578ea8
Sebastien Najjar <sebastien.najjar@gmail.com>
2011-11-01 05:02:42 -0700

    encapsule xml parsing in an object

--- visualvalgrind/src/visualvalgrind.py
+++ visualvalgrind/src/visualvalgrind.py
@@ -30,12 +30,12 @@ class ValgrindDOTExporter(DOTExporter):
         src_name = arrow.get_src_node().get_name() 
         dst_name = arrow.get_dst_node().get_name() 
         if arrow.has_attr("color"):
-           self.f.write(src_name + " -> " + dst_name +
-                " [label="+str(arrow.get_attr("leak")) + ", "\
-                "color="+str(arrow.get_attr("color"))+"]\n")
+            self.f.write(src_name + " -> " + dst_name +
+                    " [label="+str(arrow.get_attr("leak")) + ", "\
+                            "color="+str(arrow.get_attr("color"))+"]\n")
         else: 
             self.f.write(src_name + " -> " + dst_name +
-                " [label="+str(arrow.get_attr("leak")) + "]\n")
+                    " [label="+str(arrow.get_attr("leak")) + "]\n")
 
     def export_node(self, node):
         name = node.get_name() if not demangle else "\"" + node.get_attr("demangled") + "\""
@@ -49,196 +49,192 @@ class ValgrindDOTExporter(DOTExporter):
 #
 ###############################################################################
 
-# init some values
-leakedbytes = "0"  # init that value
-in_leakedbytes = False
-kind = ""
-in_kind = False
-undefined=1
+class XMLParser:
+    def __init__(self):
+        self.leakedbytes = "0"  # init that value
+        self.in_leakedbytes = False
+        self.kind = ""
+        self.in_kind = False
+        self.undefined=1
 
-# build graph name and attributes
-def compute_node_name_attr(func):
-    global writeFileName
-    global undefined
-    shape = "note"
-    fontsize = "9"
-    font = ""
-    fname = func[0]
-    ffile = func[3]
-    fline = func[2]
+    # build graph name and attributes
+    def compute_node_name_attr(self, func):
+        global writeFileName
+        shape = "note"
+        fontsize = "9"
+        font = ""
+        fname = func[0]
+        ffile = func[3]
+        fline = func[2]
 
-    # don't loose track of undefined function, make them have a different name
-    if fname=="":
-        fname="<undefined "+str(undefined)+">"
-        undefined += 1
+        # don't loose track of undefined function, make them have a different name
+        if fname=="":
+            fname="<undefined "+str(self.undefined)+">"
+            self.undefined += 1
 
-    # if user wants file and line number, give it to him
-    if writeFileName:
-        label=fname
-        if ffile!="" and fline!="":
-            label+="\\n"+ffile+":"+fline
+        # if user wants file and line number, give it to him
+        if writeFileName:
+            label=fname
+            if ffile!="" and fline!="":
+                label+="\\n"+ffile+":"+fline
+            else:
+                label = fname
         else:
             label = fname
-    else:
-        label = fname
-    return "\"" + label + "\""
+        return "\"" + label + "\""
 
-# process callstack add the callstack to the good graph, depending on the kind
-# of error. If the kind is new, a new graph is created
-def process_callstack(cs):
-    global g, depthMax
+    # process callstack add the callstack to the good graph, depending on the kind
+    # of error. If the kind is new, a new graph is created
+    def process_callstack(self,cs):
+        global g, depthMax
 
-    if kind=="":
-        return
-
-    # search if the kind exists and if so, add to the graph
-    for graph in g:
-        if graph[0] == cs.kind:
-            graph[1].addCallstack(cs)
+        if self.kind=="":
             return
 
-    # if the kind doesn't exist yet, create it
-    graph = Callgraph(depthMax) 
-    graph.addCallstack(cs)
-    g.append( [cs.kind, graph] )
+        # search if the kind exists and if so, add to the graph
+        for graph in g:
+            if graph[0] == cs.kind:
+                graph[1].addCallstack(cs)
+                return
 
-def begin_stack():
-    global parsing, call, stack, in_call, in_stack
-    in_stack = True 
-    stack = [] 
+        # if the kind doesn't exist yet, create it
+        graph = Callgraph(depthMax) 
+        graph.addCallstack(cs)
+        g.append( [cs.kind, graph] )
 
-def end_stack():
-    global stack, in_stack, leakedbytes
-    global kind
-    in_stack = False
-    # build the call stack and add it to graph
-    func_stack = []
-    for func in stack:
-        node_name = compute_node_name_attr(func)
-        func_stack.append(node_name)
-    func_stack.insert(0, "ROOT")
-    cs = Callstack(kind, func_stack, int(leakedbytes))
-    process_callstack(cs)
-    stack = []
+    def begin_stack(self):
+        self.in_stack = True 
+        self.stack = [] 
 
-def begin_call():
-    global call, in_call
-    in_call = True
-    call = ['' for i in xrange(4)]
+    def end_stack(self):
+        self.in_stack = False
+        # build the call stack and add it to graph
+        func_stack = []
+        for func in self.stack:
+            node_name = self.compute_node_name_attr(func)
+            func_stack.append(node_name)
+        func_stack.insert(0, "ROOT")
+        cs = Callstack(self.kind, func_stack, int(self.leakedbytes))
+        self.process_callstack(cs)
+        self.stack = []
 
-def end_call():
-    global stack, call, in_call
-    in_call = False
-    stack.append(call)
-    call = [] 
+    def begin_call(self):
+        self.in_call = True
+        self.call = ['' for i in xrange(4)]
 
-def begin_error():
-    global in_error
-    in_error = True
+    def end_call(self):
+        self.in_call = False
+        self.stack.append(self.call)
+        self.call = [] 
 
-def end_error():
-    global in_error, leakedbytes
-    in_error = False 
-    leakedbytes = 0
+    def begin_error(self):
+        self.in_error = True
 
-def begin_leakedbytes():
-    global in_leakedbytes
-    in_leakedbytes = True
+    def end_error(self):
+        self.in_error = False 
+        self.leakedbytes = 0
 
-def end_leakedbytes():
-    global in_leakedbytes
-    in_leakedbytes = False 
+    def begin_leakedbytes(self):
+        self.in_leakedbytes = True
 
-def begin_kind():
-    global in_kind
-    in_kind = True
+    def end_leakedbytes(self):
+        self.in_leakedbytes = False 
 
-def end_kind():
-    global in_kind
-    in_kind = False
+    def begin_kind(self):
+        self.in_kind = True
+
+    def end_kind(self):
+        self.in_kind = False
 
 
-def add_call_attribute(value):
-    global call, parsing, demangle
-    if parsing=="fn": 
-        # demangle the function name
-        fn = value.strip(" \r\n")
-        if len(fn)>truncateVal:
-            fn = fn[:truncateVal]+".."
-        call[0]= fn
-    if parsing=="dir": 
-        call[1]=value
-    if parsing=="line": 
-        call[2]=value
-    if parsing=="file": 
-        call[3]=value
+    def add_call_attribute(self, value):
+        if self.parsing=="fn": 
+            # demangle the function name
+            fn = value.strip(" \r\n")
+            if len(fn)>truncateVal:
+                fn = fn[:truncateVal]+".."
+            self.call[0]= fn
+        if self.parsing=="dir": 
+            self.call[1]=value
+        if self.parsing=="line": 
+            self.call[2]=value
+        if self.parsing=="file": 
+            self.call[3]=value
 
 
-# 3 handlers that are called when parsing xml
+    # 3 handlers that are called when parsing xml
+    def xml_start_element(self, name, attr):
+        self.parsing = name
+        if name=="stack":
+            self.begin_stack()
+        if name=="frame":
+            self.begin_call()
+        if name=="error":
+            self.begin_error()
+        if name=="leakedbytes":
+            self.begin_leakedbytes()
+        if name=="kind":
+            self.begin_kind()
+
+    def xml_handle_data(self, data):
+        if data=='\n':
+            return
+
+        stripped_data = data.strip(" \r\n")
+        if self.in_call:
+            if stripped_data != "":
+                self.add_call_attribute(stripped_data)
+        if self.in_leakedbytes:
+            self.leakedbytes = stripped_data
+        if self.in_kind:
+            self.kind = stripped_data
+
+    def xml_end_element(self, name):
+        self.parsing = ""
+        if name=="stack":
+            self.end_stack()
+        if name=="frame":
+            self.end_call()
+        if name=="error":
+            self.end_error()
+        if name=="leakedbytes":
+            self.end_leakedbytes()
+        if name=="kind":
+            self.end_kind()
+
+    def importXmlFile(self, fname):
+        # init the parser
+        p = xml.parsers.expat.ParserCreate()
+        p.StartElementHandler = xml_start_element
+        p.EndElementHandler = xml_end_element
+        p.CharacterDataHandler = xml_handle_data
+
+        # first round just to create all global var
+        self.begin_stack()
+        self.begin_call()
+        self.end_call()
+        self.end_stack()
+
+        # parse the file 
+        f = open(fname)
+        xmlContent = f.read()
+        xmlContent = re.sub("\&.*;", "", xmlContent) # remove &amp; and stuff like that
+        p.Parse(xmlContent)
+        try:
+            None
+        except:
+            print "Error: the file " + fname + " is not well-formed"
+        f.close()
+
 def xml_start_element(name, attr):
-    global parsing
-    parsing = name
-    if name=="stack":
-        begin_stack()
-    if name=="frame":
-        begin_call()
-    if name=="error":
-        begin_error()
-    if name=="leakedbytes":
-        begin_leakedbytes()
-    if name=="kind":
-        begin_kind()
+    xmlparse.xml_start_element(name, attr)
 
 def xml_handle_data(data):
-    global in_call, in_leakedbytes, leakedbytes, in_kind, kind
-    if data=='\n':
-        return
-    
-    stripped_data = data.strip(" \r\n")
-    if in_call:
-        if stripped_data != "":
-            add_call_attribute(stripped_data)
-    if in_leakedbytes:
-        leakedbytes = stripped_data
-    if in_kind:
-        kind = stripped_data
+    xmlparse.xml_handle_data(data)
 
 def xml_end_element(name):
-    global parsing
-    parsing = ""
-    if name=="stack":
-        end_stack()
-    if name=="frame":
-        end_call()
-    if name=="error":
-        end_error()
-    if name=="leakedbytes":
-        end_leakedbytes()
-    if name=="kind":
-        end_kind()
+    xmlparse.xml_end_element(name)
 
-def importXmlFile(fname):
-    # init the parser
-    p = xml.parsers.expat.ParserCreate()
-    p.StartElementHandler = xml_start_element
-    p.EndElementHandler = xml_end_element
-    p.CharacterDataHandler = xml_handle_data
-
-    # first round just to create all global var
-    begin_stack()
-    begin_call()
-    end_call()
-    end_stack()
-
-    # parse the file 
-    f = open(fname)
-    xmlContent = f.read()
-    xmlContent = re.sub("\&.*;", "", xmlContent) # remove &amp; and stuff like that
-    try:
-        p.Parse(xmlContent)
-    except:
-        print "Error: the file " + fname + " is not well-formed"
-    f.close()
 
 #
 #  call class
@@ -399,14 +395,14 @@ class Callgraph:
         for n in self.g.nodes:
             s += n.strip("\"") + "\n"
         demangled_nodes = subprocess.Popen(["c++filt", "-p"],
-                                           stdin=subprocess.PIPE,
-                                           stdout=subprocess.PIPE).communicate(s)[0]
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE).communicate(s)[0]
         demangled_nodes = demangled_nodes.split("\n")
-        
+
         # add parameter to each node
         for i,node_name in enumerate(self.g.nodes):
             self.g.get_node(node_name).add_attr("demangled", demangled_nodes[i])
-    
+
     def draw(self, fname="leak.dot", node="ROOT"):
         global output_dir, svg
         # create dot file
@@ -438,12 +434,12 @@ class Callgraph:
                         f.write(line.replace("graph0","viewport"))
                         firstg = False
                 else:
-                   f.write(line)
+                    f.write(line)
             f.close()      
             subprocess.call(["rm", path+".svg.bak"])
-                
 
-            
+
+
 
 
 
@@ -452,23 +448,16 @@ class Callgraph:
 #
 ##############################################################################
 
-
 def init_g():
-    global leakedbytes, in_leakedbytes, kind, in_kind, undefined
-    global g
+    global g, xmlparse
     g = []
+    xmlparse = XMLParser()
 
-    # init parser values
-    leakedbytes = "0"  # init that value
-    in_leakedbytes = False
-    kind = ""
-    in_kind = False
-    undefined=1
-        
 
 def add_files(files):
+    global xmlparse
     for f in files: 
-        importXmlFile(f)
+        xmlparse.importXmlFile(f)
 
 def print_graph(g):
     for graph in g:
@@ -527,20 +516,20 @@ import argparse
 parser = argparse.ArgumentParser(prog='visualvalgrind')
 # common arguments
 parser.add_argument('-finfo', action='store_true', default=False,
-                    dest='finfo',
-                    help='write file name and line numbers')
+        dest='finfo',
+        help='write file name and line numbers')
 parser.add_argument('-depth', action='store', dest='depth', type=int,
-                    default=12, help='maximal depth of the graph')
+        default=12, help='maximal depth of the graph')
 parser.add_argument('-t', action='store', dest='truncate', type=int,
-                    default=50, help='maximal length of symbols')
+        default=50, help='maximal length of symbols')
 parser.add_argument('-o', '--output-dir', action='store', dest='output_dir',
-                    help='change output directory')
+        help='change output directory')
 parser.add_argument('-d', action='store_true', default=False,
-                    dest='demangle',
-                    help='demangle symbols')
+        dest='demangle',
+        help='demangle symbols')
 parser.add_argument('-svg', action='store_true', default=False,
-                    dest='svg',
-                    help='output files in SVG format instead of DOT')
+        dest='svg',
+        help='output files in SVG format instead of DOT')
 subparsers = parser.add_subparsers(help='sub-command help')
 
 #sub-command "build"
@@ -556,9 +545,9 @@ parser_diff.add_argument('-old', action="store", dest="difffiles", nargs='+', he
 # -i and -r are exclusive
 diff_mode = parser_diff.add_mutually_exclusive_group()
 diff_mode.add_argument('-i', action='store_true', default=False,
-                    dest='diffonly', help='only display leaks that have increased')
+        dest='diffonly', help='only display leaks that have increased')
 diff_mode.add_argument('-r', action='store', dest='ratio', type=float,
-                    default=0, help='only display leaks that have increased by at least <ratio> times')
+        default=0, help='only display leaks that have increased by at least <ratio> times')
 parser_diff.set_defaults(func=diff_cmd)
 
 
