443706412b79a99c10cdaf3b548331dfb2f908d8
Sebastien Najjar <sebastien.najjar@gmail.com>
2011-11-01 04:25:34 -0700

    Fix fast demangling option

---
+++ visualvalgrind/test/regression/007_option_demangle/001_basic/validate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# find out the directory where this script is
+DIR="$PWD/$(dirname ${BASH_SOURCE[@]})/../../../../.."
+REALDIR=$(readlink -f $DIR)
+LIBDIR="$REALDIR/visualvalgrind/libs"
+
+
+rm -f *.dot 2>&1 > /dev/null
+PYTHONPATH="$LIBDIR" ../../../../src/visualvalgrind.py -d build valtest.xml
+for file in $(ls *.dot)
+do
+    expected="$(basename $file .dot).expected"
+    test=$(diff $file $expected)
+
+    if [ "$test" != "" ]; then
+        echo "0"
+        exit 1
+    fi
+done
+echo "1"
+
+rm -f *.dot 2>&1 > /dev/null
